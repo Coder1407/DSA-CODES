a46e9feda26900883e1268ad3b6d4fdb8601d38f
Coder1407 <121633636+Coder1407@users.noreply.github.com>
2023-07-03 16:04:55 +0530

    Add files via upload

---
+++ 8. Conditional Statements/II_Largest.java
@@ -0,0 +1,10 @@
+public class II_Largest {
+    public static void main(String args[]) {
+        int A = 1;
+        int B = 3;
+        if(A>=B)
+        System.out.println(A);
+        else
+        System.out.println(B);
+    }
+}
